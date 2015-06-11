d8494d5875653a55bfcf9490ba365d7a64a6f3cf
yuting pang <4yutingpang@gmail.com>
2015-06-10 20:17:56 -0700

    updated app.py

--- www/user.py
+++ www/user.py
@@ -2,7 +2,7 @@ import orm
 from models import User, Blog, Comment
 
 def test():
-    yield from orm.create_pool(user='py3_webapp', password='py3_webapp', database='awesome')
+    yield from orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='py3_webapp', password='py3_webapp', database='awesome')
 
     u = User(name='Test', email='test@example.com', passwd='123', image='about:blank')
 
